4cf85cdf1471d7017beb4f5e1514a86449dd9aa9
AbdulWahab15 <awusmani30@gmail.com>
2019-07-01 08:27:54 +0500

    practise

---
+++ lib/UI/list.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Lists extends StatefulWidget
+{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return Stateful_State();
+  }
+
+
+}
+
+class Stateful_State extends State<Lists>{
+  Widget _myListView(BuildContext context) {
+    return ListView.separated(
+      itemCount: 500,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text('row $index'),
+        );
+      },
+      separatorBuilder: (context,index){
+        return Divider();
+      },
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body:
+      Container(
+        decoration:BoxDecoration(
+        image: DecorationImage(image: AssetImage("images/splash.jpg"),
+        fit: BoxFit.cover),
+        ),
+        child: _myListView(context),
+      )
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import './UI/list.dart';
+void main(){
+  runApp(MaterialApp(
+    title: "ListView",
+    home: Lists(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
